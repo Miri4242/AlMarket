29ee161f99dc048c58fa0d2c08dcd1c5a8c284d9
Miri4242 <miryusifbabayev42@gmail.com>
2025-04-28 23:15:12 +0400

    Comit Init

--- app/src/main/java/com/example/almarket/sehifeler/Shopping.kt
+++ app/src/main/java/com/example/almarket/sehifeler/Shopping.kt
@@ -2,7 +2,9 @@ package com.example.almarket.sehifeler
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.*
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
@@ -10,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -18,11 +21,26 @@ import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.almarket.model.Product
 import com.example.almarket.viewmodel.ProductViewModel
+import java.io.FilterReader
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ShoppingMain(navController: NavController? = null, viewModel: ProductViewModel = viewModel()) {
     val productList by viewModel.products.observeAsState(initial = emptyList())
+
+    val categories = remember(productList) {
+        productList.map { it.category }.distinct()
+    }
+
+    var selectedCategory by remember { mutableStateOf<String?>(null) }
+
+    val filteredProducts = remember(productList, selectedCategory) {
+        if (selectedCategory == null) {
+            productList
+        } else {
+            productList.filter { it.category == selectedCategory }
+        }
+    }
 
     Scaffold(
         topBar = {
@@ -32,18 +50,42 @@ fun ShoppingMain(navController: NavController? = null, viewModel: ProductViewMod
             MyAppBottomBar(navController)
         }
     ) { padding ->
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            modifier = Modifier
-                .padding(padding)
-                .fillMaxSize(),
-            contentPadding = PaddingValues(8.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            horizontalArrangement = Arrangement.spacedBy(12.dp)
-        ) {
-            items(productList) { product ->
-                ProductCard(product = product)
+        Column (
+            modifier = Modifier.fillMaxSize()
+                .padding(top = 90.dp, bottom = 130.dp)
+        ){
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                item{
+                    FilterChip(
+                        selected = selectedCategory == null,
+                        onClick = { selectedCategory = null},
+                        label = { Text("Bütün məhsullar")}
+                    )
+                }
+                items(categories){
+                    category ->
+                    FilterChip(
+                        selected = selectedCategory == category,
+                        onClick = { selectedCategory = category},
+                        label = {Text(category)}
+                    )
+                }
             }
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                contentPadding = PaddingValues(8.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                items(filteredProducts) { product ->
+                    ProductCard(product = product)
+            }
+        }
         }
     }
 }
@@ -57,49 +99,49 @@ fun ProductCard(product: Product) {
         elevation = CardDefaults.cardElevation(6.dp)
     ) {
         Column(modifier = Modifier.padding(12.dp)) {
-//            AsyncImage(
-//                model = ImageRequest.Builder(LocalContext.current)
-//                    .data(product.image)
-//                    .crossfade(true)
-//                    .build(),
-//                contentDescription = product.title,
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .height(140.dp)
-//            )
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(product.image)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = product.title,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(120.dp)
+            )
 
             Spacer(modifier = Modifier.height(8.dp))
 
             Text(
                 text = product.title,
-                fontSize = 16.sp,
+                fontSize = 18.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.Red
             )
 
             Text(
                 text = product.category,
-                fontSize = 13.sp,
+                fontSize = 14.sp,
                 color = Color.Red
             )
 
             Spacer(modifier = Modifier.height(4.dp))
 
             Card (
-                modifier = Modifier
-                    .height(25.dp),
                 elevation = CardDefaults.cardElevation(6.dp),
                 colors = CardDefaults.cardColors(
-                    containerColor = Color.Yellow
+                    containerColor = Color.Green
                 )
             ){
                 Text(
-                    text = "₺${product.price}",
-                    fontSize = 15.sp,
-                    fontWeight = FontWeight.Thin,
-                    color = Color.Gray
+                    text = "₼${product.price}",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    modifier = Modifier.padding(top = 6.dp, bottom = 6.dp, start = 12.dp, end = 12.dp)
                 )
             }
         }
     }
 }
+
